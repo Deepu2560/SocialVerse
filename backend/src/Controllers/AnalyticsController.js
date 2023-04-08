6c156f2d99dae6304903347329f729ef1ce350b4
unknown <deepanshugulia256@gmail.com>
2023-04-08 22:28:52 +0530

    deploying backend on cyclic

--- backend/src/Controllers/AnalyticsController.js
+++ backend/src/Controllers/AnalyticsController.js
@@ -21,7 +21,7 @@ router.get("/users", async (req, res) => {
     });
   } catch (err) {
     console.log(
-      "=>> ERROR: while get analytics of total number or users:",
+      "=>> ERROR: while get analytics of total number of users:",
       err.message,
     );
     res.status(400).send({ error: true, message: "Bad request" });
@@ -46,8 +46,6 @@ router.get("/users/top-active", async (req, res) => {
           name: 1,
           email: 1,
           bio: 1,
-          created_at: 1,
-          updated_at: 1,
           postCount: { $size: "$posts" },
         },
       },
@@ -65,6 +63,48 @@ router.get("/users/top-active", async (req, res) => {
   } catch (err) {
     console.log(
       "Error: while getting the top 5 active users analytics:",
+      err.message,
+    );
+    res.status(400).send({ error: true, message: "Bad request" });
+  }
+});
+
+// Retrieve the total number of posts
+router.get("/analytics/posts", async (req, res) => {
+  try {
+    const totalPosts = await PostModel.countDocuments();
+
+    console.log("Getting the count of total number of posts");
+
+    res.status(200).send({
+      error: false,
+      message: "Got total number of posts",
+      data: totalPosts,
+    });
+  } catch (err) {
+    console.log(
+      "=>> ERROR: while get analytics of total number of posts:",
+      err.message,
+    );
+    res.status(400).send({ error: true, message: "Bad request" });
+  }
+});
+
+// Retrieve the top 5 most liked posts
+router.get("/analytics/posts/top-liked", async (req, res) => {
+  try {
+    const topLikedPosts = await PostModel.find().sort({ likes: -1 }).limit(5);
+
+    console.log("=>> Getting analytics of top 5 active users");
+
+    res.status(200).send({
+      error: false,
+      message: "Got top 5 active users",
+      data: topLikedPosts,
+    });
+  } catch (err) {
+    console.log(
+      "Error: while getting the top 5 liked posts analytics:",
       err.message,
     );
     res.status(400).send({ error: true, message: "Bad request" });
