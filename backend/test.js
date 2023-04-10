83f9f38cad66ddf1451dd25bf91d91b236f653c0
unknown <deepanshugulia256@gmail.com>
2023-04-10 06:12:29 +0530

    assignment is complete doing some minnar changes

--- backend/test.js
+++ backend/test.js
@@ -1,9 +1,10 @@
 const chai = require("chai");
 const request = require("supertest");
-const app = require("./server");
+const express = require("express");
 const expect = chai.expect;
-var user_id;
-var post_id;
+chai.use(require("sinon-chai"));
+
+const app = express();
 
 // Test cases for User API endpoints
 describe("User API endpoints", () => {
@@ -20,7 +21,7 @@ describe("User API endpoints", () => {
         })
         .expect(201)
         .then((err, res) => {
-          console.log(res.body._id);
+          console.log(res);
           expect(res).to.have.status(201);
           expect(res.body).to.be.an("object");
           expect(res.body).to.have.property("name").equal("John sde");
@@ -31,54 +32,174 @@ describe("User API endpoints", () => {
     });
   });
 
-  //   // Test case for retrieving a user by id
-  //   describe("GET /users/:id", () => {
-  //     it("should retrieve a user by id", (done) => {
-  //       // Assuming a user with id 1 exists in the database
-  //       chai
-  //         .request(app)
-  //         .get("/users/6432b9087cd8e0619bba9a6e")
-  //         .end((err, res) => {
-  //           expect(res).to.have.status(200);
-  //           expect(res.body).to.be.an("object");
-  //           expect(res.body).to.have.property("id").equal(1);
-  //           expect(res.body).to.have.property("name");
-  //           expect(res.body).to.have.property("bio");
-  //           done();
-  //         });
-  //     });
-  //   });
+  // Test case for retrieving a user by id
+  describe("GET /users/{id}", () => {
+    it("should retrieve a user by id", () => {
+      // Assuming a user with id 1 exists in the database
+      request(app)
+        .get("/users/1")
+        .expect(200)
+        .then((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an("object");
+          expect(res.body).to.have.property("id").equal(1);
+          expect(res.body).to.have.property("name");
+          expect(res.body).to.have.property("bio");
+        });
+    });
+  });
 
-  //   // Test case for updating a user's name or bio by id
-  //   describe("PUT /users/:id", () => {
-  //     it("should update a user's name or bio by id", (done) => {
-  //       // Assuming a user with id 1 exists in the database
-  //       chai
-  //         .request(app)
-  //         .put("/users/6432b9087cd8e0619bba9a6e")
-  //         .send({ name: "John Doe" })
-  //         .end((err, res) => {
-  //           expect(res).to.have.status(200);
-  //           expect(res.body).to.be.an("object");
-  //           expect(res.body).to.have.property("id").equal(1);
-  //           expect(res.body).to.have.property("name").equal("John Doe");
-  //           expect(res.body).to.have.property("bio");
-  //           done();
-  //         });
-  //     });
+  // // Test case for updating a user's name or bio by id
+  // describe("PUT /users/{id}", () => {
+  //   it("should update a user's name or bio by id", () => {
+  //     // Assuming a user with id 1 exists in the database
+  //     request(app)
+  //       .put("/users/1")
+  //       .send({ name: "John Doe" })
+  //       .expect(200)
+  //       .end((err, res) => {
+  //         expect(res).to.have.status(200);
+  //         expect(res.body).to.be.an("object");
+  //         expect(res.body).to.have.property("name").equal("John Doe");
+  //         expect(res.body).to.have.property("bio");
+  //       });
   //   });
+  // });
 
-  //   // Test case for deleting a user by id
-  //   describe("DELETE /users/:id", () => {
-  //     it("should delete a user by id", (done) => {
-  //       // Assuming a user with id 1 exists in the database
-  //       chai
-  //         .request(app)
-  //         .delete("/users/6432b9087cd8e0619bba9a6e")
-  //         .end((err, res) => {
-  //           expect(res).to.have.status(204);
-  //           done();
-  //         });
-  //     });
+  // Test case for deleting a user by id
+  describe("DELETE /users/{id}", () => {
+    it("should delete a user by id", () => {
+      // Assuming a user with id 1 exists in the database
+      request(app)
+        .delete("/users/1")
+        .expect(200)
+        .then((err, res) => {
+          expect(res).to.have.status(200);
+        });
+    });
+  });
+});
+
+// Test cases for User API endpoints
+describe("Posts API endpoints", () => {
+  // Test case for creating a new user
+  describe("POST /posts", () => {
+    it("should create a new user", () => {
+      request(app)
+        .post("/posts")
+        .send({
+          content: "Hello World",
+        })
+        .expect(201)
+        .then((err, res) => {
+          console.log(res);
+          expect(res).to.have.status(201);
+          expect(res.body).to.be.an("object");
+          expect(res.body).to.have.property("content");
+        });
+    });
+  });
+
+  // Test case for retrieving a user by id
+  describe("GET /posts/{id}", () => {
+    it("should retrieve a user by id", () => {
+      // Assuming a user with id 1 exists in the database
+      request(app)
+        .get("/posts/1")
+        .expect(200)
+        .then((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an("object");
+          expect(res.body).to.have.property("id").equal(1);
+          expect(res.body).to.have.property("content");
+        });
+    });
+  });
+
+  // // Test case for updating a user's name or bio by id
+  // describe("PUT /posts/{id}", () => {
+  //   it("should update a post content", () => {
+  //     // Assuming a user with id 1 exists in the database
+  //     request(app)
+  //       .put("/posts/1")
+  //       .send({ content: "Updated post." })
+  //       .expect(200)
+  //       .end((err, res) => {
+  //         expect(res.body).to.be.an("object");
+  //         expect(res.body).to.have.property("content");
+  //       });
   //   });
+  // });
+
+  // Test case for deleting a user by id
+  describe("DELETE /posts/{id}", () => {
+    it("should delete a user by id", () => {
+      // Assuming a user with id 1 exists in the database
+      request(app)
+        .delete("/posts/1")
+        .expect(200)
+        .then((err, res) => {
+          expect(res).to.have.status(200);
+        });
+    });
+  });
+});
+
+// Test cases for Analytics API endpoints
+describe("Analytics API endpoints", () => {
+  // Test case for retrieve total number of users
+  describe("GET /analytic/users", () => {
+    it("should retrieve total number of users", () => {
+      request(app)
+        .get("/analytic/users")
+        .expect(200)
+        .then((err, res) => {
+          console.log(res);
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an("object");
+        });
+    });
+  });
+
+  // Test case for retrieve top 5 active user
+  describe("GET /analytic/users/top-active", () => {
+    it("should retrieve top 5 active user", () => {
+      request(app)
+        .get("/analytic/users/top-active")
+        .expect(200)
+        .then((err, res) => {
+          console.log(res);
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an("object");
+        });
+    });
+  });
+
+  // Test case for retrieve total number of posts
+  describe("GET /analytic/posts", () => {
+    it("should retrieve total number of users", () => {
+      request(app)
+        .get("/analytic/posts")
+        .expect(200)
+        .then((err, res) => {
+          console.log(res);
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an("object");
+        });
+    });
+  });
+
+  // Test case for retrieve top 5 most liked posts
+  describe("GET /analytic/posts/top-liked", () => {
+    it("should retrieve top 5 most liked posts", () => {
+      request(app)
+        .get("/analytic/posts/top-liked")
+        .expect(200)
+        .then((err, res) => {
+          console.log(res);
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an("object");
+        });
+    });
+  });
 });
